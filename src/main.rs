ac42e25b3b4f048f9af743bc33231345f5001560
Douglas Wu <wckdouglas@gmail.com>
2020-12-09 13:27:20 -0500

    updated

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod utils;
 use std::string::String;
 
 fn main(){
-    let txome = transcriptome::Transcriptome::new(String::from("test/data/test.refFlat"));
-    println!("{}", txome.transcriptome("MIR6859-1").chrom)
+    let mut txome = transcriptome::Transcriptome(String::from("test/data/test.refFlat"));
+    let gene_name = String::from("SNX3");
+    //let gene = txome.fetch_gene(&gene_name);
+    //let transcript = &gene.transcripts[0].rnaId;
+    //println!("{}", transcript);
 }
